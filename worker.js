c42c6cfd2059efdec52ace82d2aafb482a739b00
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-07-04 19:13:59 +1000

    Added extra comments. Renamed directory.

--- worker.js
+++ worker.js
@@ -2,10 +2,10 @@ var fs = require('fs');
 var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
-var dummyData = require('./lib/dummy-data');
-var accessControl = require('./lib/access-control');
-var authentication = require('./lib/authentication');
-var realtimeRest = require('./lib/realtime-rest');
+var dummyData = require('./sc_modules/dummy-data');
+var accessControl = require('./sc_modules/access-control');
+var authentication = require('./sc_modules/authentication');
+var realtimeRest = require('./sc_modules/realtime-rest');
 
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
@@ -18,6 +18,11 @@ module.exports.run = function (worker) {
   app.use(serveStatic(path.resolve(__dirname, 'public')));
   httpServer.on('request', app);
   
+  /*
+    Here we attach some modules to scServer - Each module injects their own logic into the scServer to handle
+    a specific aspect of the system/business logic.
+  */
+  
   // Add some dummy data to our store
   dummyData.attach(scServer);
   
@@ -28,6 +33,11 @@ module.exports.run = function (worker) {
     In here we handle our incoming realtime connections and listen for events.
   */
   scServer.on('connection', function (socket) {
+    /*
+      Attach some modules to the socket object - Each one decorates the socket object with
+      additional features or business logic.
+    */
+  
     // Authentication logic
     authentication.attach(scServer, socket);
     
