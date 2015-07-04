e17702c889be16341af5fa7a00f6bd2679ae942e
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-07-04 23:34:09 +1000

    WIP: Improving support for lists/collections

--- sc_modules/realtime-rest.js
+++ sc_modules/realtime-rest.js
@@ -24,14 +24,12 @@ module.exports.attach = function (scServer, socket) {
   });
   
   socket.on('set', function (query, callback) {
-    // TODO: Allow saving whole objects or an entire collection
     var deepKey = [query.type, query.id];
     if (query.field) {
       deepKey.push(query.field);
     }
     scServer.global.set(deepKey, query.value, function (err) {
       if (!err) {
-        var channelName;
         if (query.field) {
           var publishPacket = {
             value: query.value
@@ -40,6 +38,29 @@ module.exports.attach = function (scServer, socket) {
         }
       }
       callback(err);
+    });
+  });
+  
+  socket.on('add', function (query, callback) {
+    var deepKey = [query.type, query.id, query.field];
+    
+    var queryFn = function (DataMap) {
+      var nextId = DataMap.count(deepKey) + 1;
+      DataMap.add(deepKey, nextId);
+      return {id: nextId, list: DataMap.get(deepKey)};
+    };
+    queryFn.data = {
+      deepKey: deepKey
+    };
+    
+    scServer.global.run(queryFn, function (err, result) {
+      if (!err) {
+        var publishPacket = {
+          value: result.list
+        };
+        scServer.global.publish(query.type + '/' + query.id + '/' + query.field, publishPacket);
+      }
+      callback(err, result.id);
     });
   });
 };
