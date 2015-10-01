5076161143c448cbe340f48a9d7b108484be743c
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2015-10-02 00:57:37 +1000

    Made it work using sc-crud-rethink using RethinkDB as database

--- sc_modules/dummy-data.js
+++ sc_modules/dummy-data.js
@@ -1,11 +1,28 @@
-module.exports.attach = function (scServer) {
+module.exports.attach = function (scServer, scCrudRethink) {
   /*
-    Note that we are using SC's default in-memory data store (scServer.global) for simplicity 
+    Note that we are using SC's default in-memory data store (scServer.global) for simplicity
     but in a real scenario, you should use a proper database to store persistent data.
   */
-  
+
+  // TODO: Add data if it doesn't already exist
+
+  // scCrudRethink.create({
+  //   type: 'Category',
+  //   value: {
+  //     "desc":  "KKKKKK" ,
+  //     "name":  "Smartphones2222"
+  //   }
+  // });
+
+  // scCrudRethink.delete({
+  //   type: 'Category',
+  //   id: "cbb69ddc-7ea0-4738-af31-e8f42d635f9b"
+  // });
+
+  var rethinkAdd = false;
+
   // Hardcoded dummy data
-  
+
   var schema = {
     Category: {
       foreignKeys: {
@@ -13,60 +30,85 @@ module.exports.attach = function (scServer) {
       }
     }
   };
-  
+
   scServer.global.set('Schema', schema);
-  
+
   var categories = {
     1: {
-      id: 1,
       name: 'Smartphones',
-      desc: 'Handheld mobile devices',
-      products: [1, 2]
+      desc: 'Handheld mobile devices'
     },
     2: {
-      id: 2,
       name: 'Tablets',
       desc: 'Mobile tablet devices'
     },
     3: {
-      id: 3,
       name: 'Desktops',
       desc: 'Desktop computers'
     },
     4: {
-      id: 4,
       name: 'Laptops',
       desc: 'Laptops computers'
     }
   };
-  
+
   scServer.global.set('Category', categories);
-  
+
+  if (rethinkAdd) {
+    Object.keys(categories).forEach(function (id) {
+      var obj = categories[id];
+      scCrudRethink.create({
+        type: 'Category',
+        value: obj
+      });
+    });
+  }
+
   // Hardcoded dummy data
   var products = {
     1: {
-      id: 1,
       name: 'Google NEXUS 6 32GB',
       qty: 6,
       price: 649.95,
-      desc: 'A smartphone by Google'
+      desc: 'A smartphone by Google',
+      category: 'a'
     },
     2: {
-      id: 2,
       name: 'Apple iPhone 6',
       qty: 14,
       price: 999.00,
-      desc: 'A smartphone by Apple'
+      desc: 'A smartphone by Apple',
+      category: 'a'
     }
   };
-  
+
   scServer.global.set('Product', products);
-  
+
+  if (rethinkAdd) {
+    Object.keys(products).forEach(function (id) {
+      var obj = products[id];
+      scCrudRethink.create({
+        type: 'Product',
+        value: obj
+      });
+    });
+  }
+
   var users = {
     'bob': {
       password: 'password123'
     }
   };
-  
+
   scServer.global.set('User', users);
+
+  if (rethinkAdd) {
+    Object.keys(users).forEach(function (id) {
+      var obj = users[id];
+      scCrudRethink.create({
+        type: 'User',
+        value: obj
+      });
+    });
+  }
 };
--- worker.js
+++ worker.js
@@ -6,29 +6,80 @@ var dummyData = require('./sc_modules/dummy-data');
 var accessControl = require('./sc_modules/access-control');
 var authentication = require('./sc_modules/authentication');
 var realtimeRest = require('./sc_modules/realtime-rest');
+var scCrudRethink = require('sc-crud-rethink');
 
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
-  
+
   var httpServer = worker.httpServer;
   var scServer = worker.scServer;
-  
+
   // Use ExpressJS to handle serving static HTTP files
   var app = require('express')();
   app.use(serveStatic(path.resolve(__dirname, 'public')));
   httpServer.on('request', app);
-  
+
   /*
     Here we attach some modules to scServer - Each module injects their own logic into the scServer to handle
     a specific aspect of the system/business logic.
   */
-  
+
+  var thinky = scCrudRethink.thinky;
+  var type = thinky.type;
+
+  var crudOptions = {
+    defaultPageSize: 5,
+    schema: {
+      Category: {
+        fields: {
+          id: type.string(),
+          name: type.string(),
+          desc: type.string()
+        }
+      },
+      Product: {
+        fields: {
+          id: type.string(),
+          name: type.string(),
+          qty: type.number().integer().optional(),
+          price: type.number().optional(),
+          desc: type.string().optional(),
+          category: type.string()
+        },
+        views: {
+          categoryView: {
+            pageSize: 5,
+            filter: function (r, categoryId) {
+              return r.row('category').eq(categoryId);
+            },
+            order: function (r) {
+              return r.asc('qty');
+            }
+          }
+        }
+      },
+      User: {
+        fields: {
+          username: type.string(),
+          password: type.string()
+        }
+      }
+    },
+
+    thinkyOptions: {
+      host: '127.0.0.1',
+      port: 28015
+    }
+  };
+
+  var crud = scCrudRethink.attach(worker, crudOptions);
+
   // Add some dummy data to our store
-  dummyData.attach(scServer);
-  
+  dummyData.attach(scServer, crud);
+
   // Access control middleware
   accessControl.attach(scServer);
-  
+
   /*
     In here we handle our incoming realtime connections and listen for events.
   */
@@ -37,15 +88,8 @@ module.exports.run = function (worker) {
       Attach some modules to the socket object - Each one decorates the socket object with
       additional features or business logic.
     */
-  
+
     // Authentication logic
     authentication.attach(scServer, socket);
-    
-    var restOptions = {
-      pageSize: 5
-    };
-    
-    // Realtime rest layer to get, set and subscribe to data changes
-    realtimeRest.attach(scServer, socket, restOptions);
   });
 };
